a522c73a43fec58add6ce5406b8a90deed8910b0
Suresh <soori_rozee@yahoo.com>
2019-04-27 12:46:18 +0530

    Mysql -> mysqli included

--- html/connect.php
+++ html/connect.php
@@ -4,7 +4,7 @@ $user='root';
 $password='mysql';
 $database='ramayana';
 
-$db = mysql_connect("localhost",$user,$password) or die("Not connected to database");
-$rs = mysql_select_db($database,$db) or die("No Database");
-mysql_query("set names utf8");
+// $db = mysql_connect("localhost",$user,$password) or die("Not connected to database");
+// $rs = mysql_select_db($database,$db) or die("No Database");
+// mysql_query("set names utf8");
 ?>
